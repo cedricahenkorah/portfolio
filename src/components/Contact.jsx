8dad7205fc6ad8401da57fac5a7964f718e90b2d
Cahenkorah <42823702+Cahenkorah@users.noreply.github.com>
2023-03-12 01:25:50 +0000

    misc

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,42 +4,41 @@ export const Contact = () => {
   return (
     <div
       name="contact"
-      className="w-full h-screen bg-[#0a192f] flex justify-center items-center p-4"
+      className="w-full h-screen bg-slate-800 flex justify-center items-center p-4"
     >
       <form
         method="POST"
-        action="https://getform.io/f/a699a1b2-f225-434e-b317-1fbbde8e006c"
+        action="https://getform.io/f/912e9d59-e380-43be-9dee-cbf6ae5419a2 "
         className="flex flex-col max-w-[600px] w-full"
       >
         <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300">
-            Contact
+          <p className="text-4xl font-bold inline border-b-4 border-amber-300 text-neutral-100">
+            Contact me
           </p>
-          <p className="text-gray-300 py-4">
-            // Submit the form below or shoot me an email -
-            vutukuri.kumar192st.niituniversity.in
+          <p className="py-4 text-neutral-100">
+            Let's get to work, shoot me an email - cedricahenkorah@gmail.com
           </p>
         </div>
         <input
-          className="bg-[#ccd6f6] p-2"
           type="text"
-          placeholder="Name"
           name="name"
+          placeholder="Your Name"
+          className="bg-neutral-100 p-2"
         />
         <input
-          className="my-4 p-2 bg-[#ccd6f6]"
           type="email"
-          placeholder="Email"
           name="email"
+          placeholder="Email address"
+          className="bg-neutral-100 p-2 my-4"
         />
         <textarea
-          className="bg-[#ccd6f6] p-2"
           name="message"
-          rows="10"
           placeholder="Message"
+          rows="10"
+          className="bg-neutral-100 p-2"
         ></textarea>
-        <button className="text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center">
-          Let's Collaborate
+        <button className="text-neutral-100 border-2 hover:text-slate-800 rounded-md hover:bg-amber-300 hover:border-amber-300 px-4 py-3 my-8 mx-auto flex items-center">
+          Let's get to work!
         </button>
       </form>
     </div>
